9e118b1c93a374e69ffc3a1e782ba089bc249f74
MightyXi <100283671+MightyXi@users.noreply.github.com>
2023-01-31 20:39:17 +0300

    Create main.java

---
+++ self-made deque/main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+//        Deque<Integer> dequetopush = new Deque<>();
+//        for (int i = 0; i < 10; i++)
+//            dequetopush.pushBack(i);
+//        Deque<Integer> deque = new Deque<>();
+//        deque.pushBackAll(dequetopush);
+//        deque.pushBack(4);
+//        System.out.println(deque);
+//        System.out.println(deque.front());
+//        System.out.println(deque.back());
+//        deque.pushFront(2);
+//        System.out.println(deque);
+//        deque.popFront();
+//        deque.popBack();
+//        System.out.println(deque);
+//        deque.pushFrontAll(dequetopush);
+//        System.out.println(deque);
+        Window window = new Window("Lab10");
+        window.setSize(600, 500);
+        window.setVisible(true);
+    }
+}
